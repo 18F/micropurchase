52b71355e000ba2e6390654cc1646ff2abfbb06e
Alan deLevie <adelevie@gmail.com>
2015-10-13 15:56:42 -0400

    Added session storage of current user

--- app.rb
+++ app.rb
@@ -11,13 +11,36 @@ class App < Sinatra::Base
              ENV['MPT_3500_GITHUB_SECRET']
   end
 
-  get '/auth/github/callback' do
-    auth = request.env['omniauth.auth']
-    puts auth
+  helpers do
+    # define a current_user method, so we can be sure if an user is authenticated
+    def current_user
+      !session[:uid].nil?
+    end
   end
 
-  get '/login' do
-    erb :login
+  before do
+    # we do not want to redirect to twitter when the path info starts
+    # with /auth/
+    pass if request.path_info =~ /^\/auth\//
+
+    # /auth/twitter is captured by omniauth:
+    # when the path info matches /auth/twitter, omniauth will redirect to twitter
+    redirect to('/auth/github') unless current_user
+  end
+
+  get '/auth/github/callback' do
+    session[:uid] = env['omniauth.auth']['uid']
+    # this is the main endpoint to your application
+    redirect to('/')
+  end
+
+  get '/auth/failure' do
+  # omniauth redirects to /auth/failure when it encounters a problem
+  # so you can implement this as you please
+  end
+
+  get '/logout' do
+    session[:uid] = nil
   end
 
   get '/' do
