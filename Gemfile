00c52762bf568100f8a53cf9a9fcef11a305016d
Alan deLevie <adelevie@gmail.com>
2015-10-13 15:40:35 -0400

    Added basic omniauth setup

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'omniauth'
+gem 'omniauth-github'
--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 require 'bundler/setup'
 require 'sinatra/base'
+require 'omniauth'
+require 'omniauth-github'
 
 class App < Sinatra::Base
+  enable :sessions
+  use OmniAuth::Builder do
+    provider :github,
+             ENV['MPT_3500_GITHUB_KEY'],
+             ENV['MPT_3500_GITHUB_SECRET']
+  end
+
+  get '/auth/github/callback' do
+    auth = request.env['omniauth.auth']
+    puts auth
+  end
+
+  get '/login' do
+    erb :login
+  end
+
   get '/' do
     erb :index
   end
