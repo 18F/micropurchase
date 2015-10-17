d8f1c2da84360c11390752f7e9d720d6a493f846
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2015-10-16 17:44:44 -0700

    Remove non RESTful routes and views
            Since I am writing specs for bids that are in nested RESTful
            form, I decided to clean house a littel and remove the other
            stub views and actions.

--- app.rb
+++ app.rb
@@ -66,16 +66,8 @@ class App < Sinatra::Base
     erb :index
   end
 
-  get '/make-bid' do
-    erb :make_bid
-  end
-
   get '/my-bids' do
     erb :my_bids
-  end
-
-  get '/bid-submitted' do
-    erb :bid_submitted
   end
 
   # -------
