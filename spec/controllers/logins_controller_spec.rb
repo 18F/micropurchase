c08ef97bfaf27471b8ec11caf323bc2af6aa8ab1
Alan deLevie <adelevie@gmail.com>
2015-10-23 16:25:21 -0400

    Updated specs for LoginsController

--- spec/controllers/logins_controller_spec.rb
+++ spec/controllers/logins_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe LoginsController, type: :controller do
-
+  describe '#index' do
+    it 'should render getting started instructions' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
 end
