341c51278b7b283d7c49cbbd9a81e9c8871f5cb3
Alan deLevie <adelevie@gmail.com>
2015-11-12 17:02:19 -0500

    Inform currently winning bidder that they are the current winning bidder.
    Spec for notifying current winning bidder
    
    Remove commented out code

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,17 +1,3 @@
-# def create_current_auction
-#   @auction = Auction.create({
-#     start_datetime: Time.now - 3.days,
-#     end_datetime: Time.now + 3.days,
-#     title: 'Oh no, fix the world',
-#     description: 'it is broken!',
-#     issue_url: 'https://github.com/18F/calc/issues/255',
-#     github_repo: 'https://github.com/18F/calc'
-#   })
-#   @bidder = User.create(github_id: 'uid', duns_number: 'duns')
-#
-#   @auction.bids.create(bidder: @bidder, amount: 1000)
-# end
-
 def create_bidless_auction
   @auction = Auction.create({
     start_datetime: Time.now - 3.days,
