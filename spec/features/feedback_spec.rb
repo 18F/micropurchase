48c15a69d7a8c4218a051b3821738c94ecb29ec9
Alan deLevie <adelevie@gmail.com>
2015-12-17 11:12:41 -0500

    Added additional footer links and sections:
    Contact Us
    - Submit Feedback
    - Email Us
    
    Participate in this project
    - 18F Homepage
    - See This Project on GitHub

--- spec/features/feedback_spec.rb
+++ spec/features/feedback_spec.rb
@@ -4,5 +4,8 @@ RSpec.feature "User is given opportunities to provide feedback", type: :feature 
   scenario "Viewing the home page" do
     visit "/"
     expect(page).to have_content("Submit Feedback")
+    expect(page).to have_content("Email Us")
+    expect(page).to have_content("18F Homepage")
+    expect(page).to have_content("See This Project on GitHub")
   end
 end
