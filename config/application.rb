75f8978a69283e72ea45ca707f61d567cada8d48
Jacob Harris <jacob.harris@gsa.gov>
2015-12-22 17:40:48 -0500

    Overriding the web_design_standards_fonts CSS
    The CSS was not being generated to properly reference fonts with digest
    strings in their paths. This seems to be a result of using the url
    instead of font-path function to reference the fonts. This fix seems to
    resolve the problem, but not sure if this is an issue with our
    configuration or the gem yet.

--- config/application.rb
+++ config/application.rb
@@ -29,10 +29,6 @@ module Micropurchase
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-     %w(images fonts).each do |dir|
-       config.assets.paths << Rails.root.join('app', 'assets', dir)
-     end
-     
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
