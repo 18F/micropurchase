7b166c445766f10c54851def493948f0feaffeae
Jacob Harris <jacob.harris@gsa.gov>
2015-12-24 11:05:20 -0500

    Add a column for DUNS number to the bids list view

--- app/models/presenter/bid.rb
+++ app/models/presenter/bid.rb
@@ -5,17 +5,25 @@ module Presenter
     end
 
     def bidder_duns_number
-      bidder.duns_number || null
+      if presenter_auction.available?
+        '[Witheld]'
+      else
+        bidder.duns_number || null
+      end
     end
 
     def bidder_name
-      if Presenter::Auction.new(auction).available?
+      if presenter_auction.available?
         '[Name witheld until the auction ends]'
       else
         bidder.name || null
       end
     end
 
+    def presenter_auction
+      @presenter_auction ||= Presenter::Auction.new(auction)
+    end
+    
     def null
       Null::NULL
     end
