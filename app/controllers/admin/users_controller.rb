1300847b13892d15af753a044b61ce2fa1093ae7
Alan deLevie <adelevie@gmail.com>
2015-12-30 12:29:04 -0500

    Admin panel displays separate tables and counts for admins and users.
    Reworded spec name to be more accurate
    
    Failing spec for counting users in /admin/users
    
    Modify specs to test for counting admins as well as users.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,9 @@ module Admin
     before_filter :require_admin
 
     def index
-      @users = User.all.map {|user| Presenter::User.new(user)}
+      all_users = User.all.map {|user| Presenter::User.new(user)}
+      @admins = all_users.select {|u| u.is_admin?}
+      @users = all_users.select {|u| !u.is_admin?}
     end
 
     def show
