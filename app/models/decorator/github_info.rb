9b30c39bd41798efdf22458e556831103f39f922
Jacob Harris <jacob.harris@gsa.gov>
2016-01-20 22:45:04 -0500

    Add a user decorator for github information

---
+++ app/models/decorator/github_info.rb
@@ -0,0 +1,44 @@
+module Decorator
+  class GithubInfo
+    def initialize(user)
+      @user = user
+    end
+
+    def self.missing
+      User.where(github_login: [nil, '']).map {|u| new(u) }
+    end
+    
+    def missing?
+      @user.github_login.blank?
+    end
+      
+    def fetch
+      return unless missing?
+      @user.github_login = github_login
+    end
+
+    def save
+      fetch
+      @user.save
+    end
+    
+    private
+
+    def github_login
+      github_response[:login]
+    end
+    
+    def github_response
+      if @user_info.nil?
+        begin
+          client = Octokit::Client.new
+          @user_info = client.user(@user.github_id)
+        rescue Octokit::Error
+          @user_info = {}
+        end
+      end
+      
+      @user_info
+    end
+  end
+end
