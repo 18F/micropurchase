4f79c993ec097d0eb609298e6a7b2de90aa5ab0b
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-01-21 13:03:15 -0800

    migrates to add type for auctions

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,6 +2,8 @@ class Auction < ActiveRecord::Base
   has_many :bids
   has_many :bidders, through: :bids
 
+  self.inheritance_column = :__disabled
+
   scope :in_reverse_chron_order, -> { order('end_datetime DESC') }
   scope :with_bids, -> { includes(:bids) }
 end
