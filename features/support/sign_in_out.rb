0aa2eca6addbbb30e49f64968f53aab901160c7e
Jacob Harris <jacob.harris@gsa.gov>
2016-02-22 17:13:04 -0500

    Omniauth mock should set username too

--- features/support/sign_in_out.rb
+++ features/support/sign_in_out.rb
@@ -3,6 +3,9 @@ OmniAuth.config.test_mode = true
 def mock_sign_in(user)
   OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
     provider: 'github',
-    uid: user.github_id
+    uid: user.github_id,
+    info: {
+      name: user.name
+    }
   )
 end
