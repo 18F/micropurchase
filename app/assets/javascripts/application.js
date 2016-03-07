01030c6656ace0aa24caebbf06ac8f43aadcf0f2
Brian Hedberg <bshslowfood@gmail.com>
2016-03-07 17:18:50 -0500

    break apart scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,36 @@
  * = require components.js
  * = require cookies.js
  * = require sam_warning.js
+ * = require jquery-accessibleMegaMenu.js
  */
+
+
+$(function(){
+	$(".nav-login").accessibleMegaMenu({
+        /* prefix for generated unique id attributes, which are required
+           to indicate aria-owns, aria-controls and aria-labelledby */
+        uuidPrefix: "accessible-megamenu",
+
+        /* css class used to define the megamenu styling */
+        menuClass: "nav-menu",
+
+        /* css class for a top-level navigation item in the megamenu */
+        topNavItemClass: "nav-item",
+
+        /* css class for a megamenu panel */
+        panelClass: "sub-nav",
+
+        /* css class for a group of items within a megamenu panel */
+        panelGroupClass: "sub-nav-group",
+
+        /* css class for the hover state */
+        hoverClass: "hover",
+
+        /* css class for the focus state */
+        focusClass: "focus",
+
+        /* css class for the open state */
+        openClass: "open"
+    })
+});
+
