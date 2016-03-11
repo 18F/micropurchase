43597ef5c0a0f20cc0f6bed237ec61b4212c3cdc
Brian Hedberg <bshslowfood@gmail.com>
2016-03-11 10:03:58 -0500

    Refine dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,50 +5,7 @@
  * = require sam_warning.js
  * = require jquery-accessibleMegaMenu.js
  * = require drawer.js
+ * = require navDropdown.js
  */
 
-
-$(function(){
-	$(".nav-login").accessibleMegaMenu({
-        /* prefix for generated unique id attributes, which are required
-           to indicate aria-owns, aria-controls and aria-labelledby */
-        uuidPrefix: "accessible-megamenu",
-
-        /* css class used to define the megamenu styling */
-        menuClass: "nav-menu",
-
-        /* css class for a top-level navigation item in the megamenu */
-        topNavItemClass: "nav-item",
-
-        /* css class for a megamenu panel */
-        panelClass: "sub-nav",
-
-        /* css class for a group of items within a megamenu panel */
-        panelGroupClass: "sub-nav-group",
-
-        /* css class for the hover state */
-        hoverClass: "hover",
-
-        /* css class for the focus state */
-        focusClass: "focus",
-
-        /* css class for the open state */
-        openClass: "open"
-    });
-
-    var profileButton = $('.header-account .nav-menu .nav-item a');
-
-    var resizeProfileMenu = function resizeProfileMenu(button) {
-        var buttonHeight = button.height();
-        $('.header-account .nav-menu .sub-nav-group').css('margin-top', 3 + buttonHeight);
-    };
-
-    resizeProfileMenu(profileButton);
-
-    $(window).on('resize', function(){
-        $('.header-account .nav-menu .sub-nav-group')
-        resizeProfileMenu(profileButton)
-    });
-
-});
 
