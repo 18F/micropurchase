288218a3dff5c0afcacd623c4c77a9ba1e07ec9e
Jacob Harris <jacob.harris@gsa.gov>
2016-03-18 13:22:48 -0400

    Remove redundant spec/feature files
    Removing spec/feature files that are redundant to Cucumber. I did this
    once before, but these files have returned because there were some
    changes to existing methods as well as some new methods added.

--- features/logging_in_and_out.feature
+++ features/logging_in_and_out.feature
@@ -64,3 +64,29 @@ Feature: logging in and out
     Then I expect to be on the profile edit page
     And I expect to see a profile form with my info
     And there should be meta tags for the edit profile form
+
+    When I fill the "Email Address" field with "doris@doogooder.com"
+    And I click on the "Submit" button
+    Then I expect to be on the home page
+    
+    When I click on the "Edit profile" link
+    Then I expect to see "doris@doogooder.com" in the "Email Address" field
+
+  Scenario: User tries to enter an invalid email address
+    Given I am a user with a verified SAM account
+    When I visit the home page
+    And I click on the "Login" button
+    Then I expect to see an "Authorize with GitHub" button
+
+    When I click on the "Authorize with GitHub" button
+    Then I expect to be on the profile edit page
+    And I expect to see a profile form with my info
+    And there should be meta tags for the edit profile form
+
+    When I fill the "Email Address" field with "doris_the_nonvalid_email_address_person"
+    And I click on the "Submit" button
+    Then I expect to see an alert that "Email is invalid"
+
+    When I visit the home page
+    And I click on the "Edit profile" button
+    Then I expect to see my email in the "Email Address" field
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -17,6 +17,10 @@ Then(/^I expect to not see my name$/) do
   expect(page).to_not have_content(@user.name)
 end
 
+Then(/^I expect to see my email address$/) do
+  expect(page).to have_content(@user.email)
+end
+
 Then(/^I expect to see a profile form$/) do
   expect(page).to have_content("Complete your account")
   expect(page).to have_content("Name")
@@ -35,7 +39,6 @@ Then(/^I expect to see a profile form with my info$/) do
   expect(field.value).to eq(@user.email)
 end
 
-
 When(/^I fill out the profile form$/) do
   @new_name = Faker::Name.name
   @new_duns = Faker::Company.duns_number
@@ -45,6 +48,26 @@ When(/^I fill out the profile form$/) do
   fill_in("user_name", with: @new_name)
   fill_in("user_duns_number", with: @new_duns)
   fill_in("user_email", with: @new_email)
+end
+
+When(/^I fill the "([^"]*)" field with "([^"]*)"$/) do |field, value|
+  fill_in(field, with: value)
+end
+
+Then(/^I expect to see "([^"]*)" in the "([^"]*)" field$/) do |value, field|
+  field = find_field(field)
+  expect(field.value).to eq(value)
+end
+
+Then(/^I expect to see my (.+) in the "([^"]*)" field$/) do |attribute, field|
+  field = find_field(field)
+  expect(field.value).to eq(@user.send(attribute))
+end
+
+Then(/^I expect to see an alert that "([^"]*)"$/) do |message|
+  within("div.usa-alert.usa-alert-error") do
+    expect(page).to have_content(message)
+  end
 end
 
 Then(/^I expect to see my changes$/) do
