b4bd2194ebc62877c93de01524e0ce5b564d6fd8
Jacob Harris <jacob.harris@gsa.gov>
2016-03-23 14:02:36 -0400

    Tests pass, but we have limited tests

--- app/models/presenter/admin_auction.rb
+++ app/models/presenter/admin_auction.rb
@@ -6,13 +6,19 @@ module Presenter
 
     def bids
       model.bids.to_a
-        .map {|bid| Presenter::Bid.new(bid) }
-        .sort_by(&:created_at)
-        .reverse
+           .map {|bid| Presenter::Bid.new(bid) }
+           .sort_by(&:created_at)
+           .reverse
     end
 
     def bid_count
       bids.size
+    end
+
+    private
+    
+    def model
+      __getobj__
     end
   end
 end
