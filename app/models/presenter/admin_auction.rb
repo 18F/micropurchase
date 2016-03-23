bab6fdf22abace1bb94b0b5665206c758675ae43
Jacob Harris <jacob.harris@gsa.gov>
2016-03-23 10:59:04 -0400

    Test for admin/auction_controller was missing

---
+++ app/models/presenter/admin_auction.rb
@@ -0,0 +1,18 @@
+module Presenter
+  class AdminAuction < SimpleDelegator
+    def bids?
+      bid_count > 0
+    end
+
+    def bids
+      model.bids.to_a
+        .map {|bid| Presenter::Bid.new(bid) }
+        .sort_by(&:created_at)
+        .reverse
+    end
+
+    def bid_count
+      bids.size
+    end
+  end
+end
