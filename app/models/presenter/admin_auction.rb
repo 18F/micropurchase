b4312ea94cb6ec4460a21e392d032904520c20bd
Jacob Harris <jacob.harris@gsa.gov>
2016-03-23 22:05:30 -0400

    Create Presenter::AdminAuction
    For now, it's just a wrapper around Presenter::Auction

--- app/models/presenter/admin_auction.rb
+++ app/models/presenter/admin_auction.rb
@@ -1,24 +1,18 @@
 module Presenter
-  class AdminAuction < SimpleDelegator
-    def bids?
-      bid_count > 0
+  class AdminAuction
+    attr_reader :auction
+
+    def initialize(auction)
+      @auction = Presenter::Auction.new(auction)
     end
 
-    def bids
-      model.bids.to_a
-           .map {|bid| Presenter::Bid.new(bid) }
-           .sort_by(&:created_at)
-           .reverse
-    end
-
-    def bid_count
-      bids.size
-    end
-
-    private
-    
-    def model
-      __getobj__
-    end
+    delegate :bids, :bids?, :bid_count, :available?, :title, :created_at, :starts_at,
+             :ends_at, :start_datetime, :end_datetime, :github_repo, :issue_url, :summary,
+             :html_summary, :description, :html_description, :delivery_deadline, :billable_to,
+             :start_price, :published, :notes, :delivery_url, :result, :cap_proposal_url,
+             :current_bid?, :awardee_paid_status, :current_bidder_name,
+             :current_bidder_duns_number, :current_bid_amount, :current_bid_time,
+             :to_param, :model_name, :to_key, :to_model, :type, :id, :read_attribute_for_serialization,
+             to: :auction
   end
 end
