c46a6073e6a0c6e2bb46b2efcac386f19d8dd011
Jacob Harris <jacob.harris@gsa.gov>
2016-03-28 09:58:40 -0400

    AuctionPresenter delegates more to AR model
    Move some of the delegation away from the Presenter::Auction model so
    that we can soon restrict some of the methods available in the
    Presenter::Auction from being a SimpleDelegator

--- app/models/presenter/admin_auction.rb
+++ app/models/presenter/admin_auction.rb
@@ -1,18 +1,25 @@
 module Presenter
   class AdminAuction
-    attr_reader :auction
+    attr_reader :auction, :pr_auction
 
     def initialize(auction)
-      @auction = Presenter::Auction.new(auction)
+      @auction = auction
+      @pr_auction = Presenter::Auction.new(auction)
     end
 
-    delegate :bids, :bids?, :bid_count, :available?, :title, :created_at, :starts_at,
-             :ends_at, :start_datetime, :end_datetime, :github_repo, :issue_url, :summary,
-             :html_summary, :description, :html_description, :delivery_deadline, :billable_to,
-             :start_price, :published, :notes, :delivery_url, :result, :cap_proposal_url,
-             :current_bid?, :awardee_paid_status, :current_bidder_name,
-             :current_bidder_duns_number, :current_bid_amount, :current_bid_time,
-             :to_param, :model_name, :to_key, :to_model, :type, :id, :read_attribute_for_serialization,
+    delegate :bids, :bids?, :bid_count, :available?, :html_summary,
+             :html_description, :current_bid?, :current_bidder_name,
+             :current_bidder_duns_number, :current_bid_amount,
+             :current_bid_time, :starts_at, :ends_at,
+             to: :pr_auction
+
+    delegate :title, :created_at,
+             :start_datetime, :end_datetime, :github_repo, :issue_url,
+             :summary, :description, :delivery_deadline, :billable_to,
+             :start_price, :published, :notes, :delivery_url, :result,
+             :cap_proposal_url, :awardee_paid_status, :to_param,
+             :model_name, :to_key, :to_model, :type, :id,
+             :read_attribute_for_serialization,
              to: :auction
   end
 end
