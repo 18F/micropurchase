86da1d5ad5ae146d3f4f2f91ef24636e8cb372f7
Brian Hedberg <bshslowfood@gmail.com>
2016-04-04 12:01:20 -0500

    page specific scripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,11 @@
 /*
  * = require vendor/jquery-1.11.3.min.js
- * = require vendor/underscore.js
- * = require vendor/d3.min.js
- * = require vendor/c3.js
- * = require vendor/jquery.waypoints.js
  * = require vendor/jquery-accessibleMegaMenu.js
  * = require components.js
  * = require cookies.js
  * = require sam_warning.js
  * = require drawer.js
  * = require navDropdown.js
- * = require previousWinners.js
  */
 
 
