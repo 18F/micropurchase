c52740cfc67bac2ac99c44d26c9cbcae6ed65fb2
Jacob Harris <jacob.harris@gsa.gov>
2016-04-12 13:13:46 -0400

    Rephrase some of the Cucumber steps (#386)
    I decided to replace some cases where it says "I expect to" with "I
    should" to better match the Cucumber specs from new features

--- features/step_definitions/single_auction_steps.rb
+++ features/step_definitions/single_auction_steps.rb
@@ -1,4 +1,4 @@
-Then(/^I expect to not see bids from other users$/) do
+Then(/^I should not see bids from other users$/) do
   @auction.bids.each do |bid|
     next if bid.bidder_id == @user.id
     amount = ApplicationController.helpers.number_to_currency(bid.amount)
