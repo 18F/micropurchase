b55f072c55034cd6e98cbb2daa770e80668a88f4
Jessie A Young <jessica.young@gsa.gov>
2016-04-18 17:34:20 -0700

    Validate format of DUNs number
    * Validate that it contains only integers
    * Validate that it is between 8 and 13 characters long
    
    https://github.com/18F/micropurchase/issues/425

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,11 @@
 FactoryGirl.define do
-  sequence :github_id do |n|
-    n
-  end
+  sequence(:github_id) { |n| n }
+  sequence(:duns_number) { |n| "1234567#{n}" }
 
   factory :user do
+    duns_number
     name { Faker::Name.name }
     email { Faker::Internet.email }
-    duns_number { Faker::Company.duns_number }
     github_id 123_456
     sam_account true
     credit_card_form_url 'https://some-website.com/pay'
