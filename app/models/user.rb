0c1f492af68f167277aaa123e7b8627d7e845c2e
Jacob Harris <jacob.harris@gsa.gov>
2016-04-19 10:44:36 -0400

    Implement validation for the credit_card_form_url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,25 @@
 class User < ActiveRecord::Base
   has_many :bids
-  validates :email, email: true, allow_blank: true
+
+  validates :email, allow_blank: true, email: true
+  validate :validate_credit_card_form
+
+  private
+
+  def validate_credit_card_form
+    return if credit_card_form_url.blank?
+
+    uri = URI.parse(credit_card_form_url)
+
+    case uri
+    when URI::HTTPS
+    # nothing to do here
+    when URI::HTTP
+      errors.add(:credit_card_form_url, 'must use HTTPS')
+    else
+      errors.add(:credit_card_form_url, 'is an invalid URL format')
+    end
+  rescue URI::InvalidURIError
+    errors.add(:credit_card_form_url, 'is an invalid URL format')
+  end
 end
