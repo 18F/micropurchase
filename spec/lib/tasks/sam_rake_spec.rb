1f91939762d6fb20fe5a4254d34c9ad4f10944d3
Jacob Harris <jacob.harris@gsa.gov>
2016-04-20 14:26:17 -0400

    Sam Rake tasks were not being tested
    For some reason, you need to call execute not invoke to run a rake task
    in a test. This was not running sam:check so the test is now failing
    because the old version of the test was only passing because it was
    mocking the wrong things.

--- spec/lib/tasks/sam_rake_spec.rb
+++ spec/lib/tasks/sam_rake_spec.rb
@@ -5,16 +5,15 @@ RSpec.describe 'rake sam:check' do
   context 'when the sam_account for the user is false' do
     it 'uses the SamAccountReckoner to determine whether the account is valid' do
       user = FactoryGirl.create(:user, sam_account: false)
-      reckoner = SamAccountReckoner.new(user)
       Micropurchase::Application.load_tasks
-      allow(SamAccountReckoner).to receive(:unreckoned).and_return([user])
-      allow(SamAccountReckoner).to receive(:new).with(user).and_return(reckoner)
-      client = double('samwise client', duns_is_in_sam?: true)
-      allow(Samwise::Client).to receive(:new).and_return(client)
+      client = double('samwise client')
+      expect(client).to receive(:duns_is_in_sam?).and_return(true)
+      expect(Samwise::Client).to receive(:new).and_return(client)
 
-      Rake::Task['sam:check'].invoke
+      Rake::Task['sam:check'].execute
 
-      expect(user.reload).to be_sam_account
+      user = User.first
+      expect(user).to be_sam_account
       expect(User.where(sam_account: false)).to be_empty
     end
   end
@@ -28,7 +27,7 @@ RSpec.describe 'rake sam:check' do
 
       expect(reckoner).to_not receive(:set)
 
-      Rake::Task['sam:check'].invoke
+      Rake::Task['sam:check'].execute
     end
   end
 end
