b7b72dbbe2b23c5ec4f5f5e6000d92449a58a5e6
stvnrlly <steven.reilly@gsa.gov>
2016-04-29 15:50:01 -0400

    fix data.gov spec

--- spec/credentials/smtp_credentials_spec.rb
+++ spec/credentials/smtp_credentials_spec.rb
@@ -6,17 +6,22 @@ describe SMTPCredentials do
       env_var_smtp_password = 'fake smtp password'
       env_var_smtp_username = 'fake smtp username'
       env_var_default_url_host = 'fake url host'
+      env_var_default_from = 'fake@fakeurl.fake'
+
       allow(ENV).to receive(:[]).with('micropurchase_smtp_smtp_password').and_return(env_var_smtp_password)
       allow(ENV).to receive(:[]).with('micropurchase_smtp_smtp_username').and_return(env_var_smtp_username)
       allow(ENV).to receive(:[]).with('micropurchase_smtp_default_url_host').and_return(env_var_default_url_host)
+      allow(ENV).to receive(:[]).with('micropurchase_smtp_default_from').and_return(env_var_default_from)
 
       password = SMTPCredentials.smtp_password
       username = SMTPCredentials.smtp_username
       url_host = SMTPCredentials.default_url_host
+      from = SMTPCredentials.default_from
 
       expect(password).to eq env_var_smtp_password
       expect(username).to eq env_var_smtp_username
       expect(url_host).to eq env_var_default_url_host
+      expect(from).to eq env_var_default_from
     end
   end
 end
--- spec/models/data_dot_gov_credentials_spec.rb
+++ spec/models/data_dot_gov_credentials_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe DataDotGovCredentials do
   describe '::api_key' do
     let(:api_key) { DataDotGovCredentials.api_key }
     it 'returns a string' do
-      data_dot_gov_api_key_from_fixture = 'fakeapikey'
+      data_dot_gov_api_key_from_fixture = 'super secret api key'
       expect(api_key).to eq(data_dot_gov_api_key_from_fixture)
     end
   end
