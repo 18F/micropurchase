942c71dbd4d63396da4a5bd7be6847d05a36c848
stvnrlly <steven.reilly@gsa.gov>
2016-04-29 14:40:12 -0400

    update tests for buildpack parser

--- spec/models/data_dot_gov_credentials_spec.rb
+++ spec/models/data_dot_gov_credentials_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DataDotGovCredentials do
   describe '::api_key' do
-    let(:api_key) { DataDotGovCredentials.api_key(force_vcap: true) }
+    let(:api_key) { DataDotGovCredentials.api_key }
     it 'returns a string' do
       data_dot_gov_api_key_from_fixture = 'fakeapikey'
       expect(api_key).to eq(data_dot_gov_api_key_from_fixture)
