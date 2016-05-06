67e870c80471b7b1e5f022d747a5c942b7e682ac
Alan deLevie <adelevie@gmail.com>
2016-05-06 10:48:39 -0400

    Cucumber features no longer make network calls.
    Implemented FakeSamApi Sinatra app w/ WebMock.

--- features/step_definitions/email_steps.rb
+++ features/step_definitions/email_steps.rb
@@ -3,7 +3,7 @@ Then(/^email notifications are sent to losing bidders$/) do
 end
 
 Then(/^I should receive an email notifying me that I did not win$/) do
-  Delayed::Worker.new.work_off
+  
   email = ActionMailer::Base.deliveries.first
   expect(email.to.first).to eq @user.email
   expect(email.body.encoded).to include(
