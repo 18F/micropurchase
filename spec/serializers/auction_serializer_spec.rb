4433b22fc9b0159f186cf99c6fe681e4b3473761
Jessie A. Young <jessieay@users.noreply.github.com>
2016-05-09 13:33:01 -0700

    Only show winning bid in the API if auction is closed (#520)
    * Only show winning bid in the API if auction is closed
    
    * Modification / revert-ish of https://github.com/18F/micropurchase/pull/519
    
    * Explicitly return nil in NullBid methods

---
+++ spec/serializers/auction_serializer_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe AuctionSerializer do
+  describe '#winning_bid' do
+    context 'auction has no bids' do
+      it 'returns null attributes' do
+        auction = create(:auction)
+
+        serializer = AuctionSerializer.new(auction)
+
+        expect(serializer.winning_bid.to_json).to eq(
+          WinningBidSerializer.new(NullBid.new).to_json
+        )
+      end
+    end
+
+    context 'auction is available' do
+      it 'returns null attributes' do
+        auction = create(:auction)
+        allow(AuctionStatus).to receive(:new).with(auction).and_return(double(available?: true))
+
+        serializer = AuctionSerializer.new(auction)
+
+        expect(serializer.winning_bid.to_json).to eq(
+          WinningBidSerializer.new(NullBid.new).to_json
+        )
+      end
+
+      context 'auction is over' do
+        it  'returns winning bid' do
+          auction = create(:auction)
+          bid = create(:bid, auction: auction)
+          allow(AuctionStatus).to receive(:new).with(auction).and_return(double(available?: false))
+
+          serializer = AuctionSerializer.new(auction)
+
+          expect(serializer.winning_bid.to_json).to eq(
+            WinningBidSerializer.new(bid).to_json
+          )
+        end
+      end
+    end
+  end
+end
