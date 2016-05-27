bf23bb49bc4e403918fcb42ed379d575f88360ca
Brian Hedberg <bshslowfood@gmail.com>
2016-05-27 10:55:35 -0500

    codeclimate

--- app/view_models/auction_list_item.rb
+++ app/view_models/auction_list_item.rb
@@ -62,16 +62,28 @@ class AuctionListItem
   def winning_bid_partial
     if over? && !auction.bids.any?
       'auctions/no_bids'
-    elsif auction.type == 'multi_bid' && over?
-      'auctions/over_winning_bid_details'
-    elsif auction.type == 'single_bid' && over?
-      'auctions/over_with_bids'
-    elsif auction.type == 'multi_bid' && available?
-      auction_available_bids_partial
-    elsif auction.type == 'single_bid' && available?
-      single_bid_bidder_partial
+    elsif auction.type == 'single_bid'
+      auction.winning_single_bid_partial
+    elsif auction.type == 'multi_bid'
+      auction.winning_multi_bid_partial
     else # future
       'components/null'
+    end
+  end
+
+  def winning_single_bid_partial
+    if over?
+      'auctions/over_with_bids'
+    elsif available?
+      single_bid_bidder_partial
+    end
+  end
+
+  def winning_multi_bid_partial
+    if over?
+      'auctions/over_winning_bid_details'
+    elsif available?
+      auction_available_bids_partial
     end
   end
 
