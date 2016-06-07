c6da3f05a44500f174068df5edec54b2cc99d7ef
Jessie A. Young <jessie.a.young@gmail.com>
2016-06-07 11:29:33 -0700

    Fix winners page (I hope!)
    * On staging, winners page works
    * On prod, it is broken
    * https://micropurchase.18f.gov/assets/source/c3.css does not exist
    * but https://micropurchase-staging.18f.gov/assets/source/c3.css does
    * We should not be directly referencing static assets -- hellooooo we
      are using rails and we <3 the asset pipeline
    * Renaming `c3.css` to `c3.scss` makes it so we are putting it in asset
      pipeline
    * I don't understand why this is the case, but confirmed behavior by
      compiling assets locally
    * The other changes here are config cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,13 @@
 Rails.application.configure do
-  config.cache_classes = false
-  config.eager_load = false
-  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.raise_delivery_errors = false
-  config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
+  config.active_support.deprecation = :log
   config.assets.debug = true
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.cache_classes = false
+  config.consider_all_requests_local = true
+  config.eager_load = false
 end
