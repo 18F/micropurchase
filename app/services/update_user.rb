ceec5c6717fa56a7bad4617eba589aa589ba96f4
Jessie A Young <jessica.young@gsa.gov>
2016-06-14 14:37:19 -0700

    Move `UpdateUser` to services dir

--- app/services/update_user.rb
+++ app/services/update_user.rb
@@ -19,7 +19,7 @@ class UpdateUser < Struct.new(:params, :current_user)
   private
 
   def update_user
-    user.assign_attributes(user_params)
+    user.assign_attributes(parsed_user_params)
     update_sam
   end
 
@@ -33,10 +33,24 @@ class UpdateUser < Struct.new(:params, :current_user)
     reckoner.delay.set!
   end
 
+  def parsed_user_params
+    user_params.each_with_object({ }) do |(key, value), hash|
+      converted_value = nil_not_empty_string(value)
+      hash[key] = converted_value
+    end
+  end
+
   def user_params
     params
       .require(:user)
       .permit(:name, :duns_number, :email, :credit_card_form_url)
-      .reject { |_key, value| value.blank? }
+  end
+
+  def nil_not_empty_string(value)
+    if value == ""
+      nil
+    else
+      value
+    end
   end
 end
