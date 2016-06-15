a8edacc6315390f92cf1fc43eb3fd9323cced643
Jessie A Young <jessica.young@gsa.gov>
2016-06-15 10:05:20 -0700

    "" -> ''

--- spec/models/login_user_spec.rb
+++ spec/models/login_user_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 describe LoginUser do
-  describe "#perform" do
-    context "user does not have github username, name, or email" do
-      it "updates from auth hash" do
+  describe '#perform' do
+    context 'user does not have github username, name, or email' do
+      it 'updates from auth hash' do
         github_id = '1234'
-        user = build(:user, github_login: nil, name: "", email: "", github_id: github_id)
+        user = build(:user, github_login: nil, name: '', email: '', github_id: github_id)
         user.save(validate: false)
         auth_hash = {
           uid: github_id,
           info: {
-            nickname: "github_username",
-            name: "Person",
-            email: "test@example.com"
+            nickname: 'github_username',
+            name: 'Person',
+            email: 'test@example.com'
           }
         }
 
@@ -21,28 +21,28 @@ describe LoginUser do
 
         user.reload
 
-        expect(user.github_login).to eq "github_username"
-        expect(user.name).to eq "Person"
-        expect(user.email).to eq "test@example.com"
+        expect(user.github_login).to eq 'github_username'
+        expect(user.name).to eq 'Person'
+        expect(user.email).to eq 'test@example.com'
       end
     end
 
-    context "user already has github username, name, and email" do
-      it "does not update email, name, does update github username" do
+    context 'user already has github username, name, and email' do
+      it 'does not update email, name, does update github username' do
         github_id = '1234'
         user = create(
           :user,
-          github_login: "Old_Username",
-          name: "Old name",
-          email: "oldemail@example.com",
+          github_login: 'Old_Username',
+          name: 'Old name',
+          email: 'oldemail@example.com',
           github_id: github_id
         )
         auth_hash = {
           uid: github_id,
           info: {
-            nickname: "New_Username",
-            name: "New name",
-            email: "test@example.com"
+            nickname: 'New_Username',
+            name: 'New name',
+            email: 'test@example.com'
           }
         }
 
@@ -51,9 +51,9 @@ describe LoginUser do
 
         user.reload
 
-        expect(user.github_login).to eq "New_Username"
-        expect(user.name).to eq "Old name"
-        expect(user.email).to eq "oldemail@example.com"
+        expect(user.github_login).to eq 'New_Username'
+        expect(user.name).to eq 'Old name'
+        expect(user.email).to eq 'oldemail@example.com'
       end
     end
 
