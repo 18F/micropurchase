5134a7f8318804ded6092cf204f30ee0abac02a2
Jessie A. Young <jessie.a.young@gmail.com>
2016-06-20 16:18:34 -0700

    Remove unnecessary assets
    * Use assets from USWDS when possible

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.version = (ENV["ASSETS_VERSION"] || "1.0")
 Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( uswds/us_flag_small )
