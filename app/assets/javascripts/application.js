b98292ec52bdf92c24bebf9834fc527def5b84e9
Jessie A. Young <jessie.a.young@gmail.com>
2016-06-20 15:59:41 -0700

    Use newest version of USWDS
    * 0.9.4
    * Installed via NPM
    * See https://github.com/18F/web-design-standards
    * Updated for Rails asset pipeline in `vendor/uswds_override`
    * Also needed to comment out the fonts file import bc it was raising
      errors despite override
    * See https://github.com/18F/us_web_design_standards_gem/pull/7
    * Remove gem, was only being used for javascript and we aren't using
      uswds js yet
    * Address some of the many many Code climate errors for our CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery-accessibleMegaMenu.js
 //= require d3.v3.min
 //= require utility.js
-//= require components.js
 //= require drawer.js
 //= require auctionToggle.js
 //= require navDropdown.js
