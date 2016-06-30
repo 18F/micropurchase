c9d4cb4b1a3eba1cc559e1dbebeb3489c417eb49
Andrew Maier <andrew.maier@gsa.gov>
2016-06-30 14:25:07 -0400

    stuff

--- javascripts/application.js
+++ javascripts/application.js
@@ -14,13 +14,25 @@ $(document).ready(function() {
     $(this).parents('.field').removeClass('is-focused')
   });
 
-  $('.site-header .signed-in').hide();
-  if (Cookies.get('mpp-signed-in')) {
-    $('.site-header .signed-in').show();
-    $('.site-header .signed-out').hide();
+  function renderSiteHeaderSignIn(){
+    if (Cookies.get('mpp-signed-in')) {
+      $('.site-header .signed-in').show();
+      $('.site-header .signed-out').hide();
+    } else {
+      $('.site-header .signed-in').hide();
+      $('.site-header .signed-out').show();
+    }
   }
 
-  $('.site-header .a-sign-in, .site-header .a-sign-up').click(function(){ Cookies.set('mpp-signed-in', 'true'); });
-  $('.site-header .a-sign-out').click(function(){ Cookies.remove('mpp-signed-in'); });
+  renderSiteHeaderSignIn();
+
+  $('.site-header .a-sign-in, .site-header .a-sign-up').click(function(){ 
+    Cookies.set('mpp-signed-in', 'true'); 
+    renderSiteHeaderSignIn()
+  });
+  $('.site-header .a-sign-out').click(function(){ 
+    Cookies.remove('mpp-signed-in'); 
+    renderSiteHeaderSignIn()
+  });
 
 });
