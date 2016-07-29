71dabd45e534583222bf04a869ba7fbe0603327f
Jessie A. Young <jessie.a.young@gmail.com>
2016-07-29 11:18:24 -0700

    Add `var` because JavaScript

--- app/assets/javascripts/deliveryDueAt.js
+++ app/assets/javascripts/deliveryDueAt.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   calculateNewDate = function() {
-    auctionEndedAt = $("#auction_ended_at").val();
-    dueInDays = $("#auction_due_in_days").val();
-    hour = $("#auction_ended_at_1i").val();
-    minute = $("#auction_ended_at_2i").val();
-    amPm = $("#auction_ended_at_3i").val();
+    var auctionEndedAt = $("#auction_ended_at").val();
+    var dueInDays = $("#auction_due_in_days").val();
+    var hour = $("#auction_ended_at_1i").val();
+    var minute = $("#auction_ended_at_2i").val();
+    var amPm = $("#auction_ended_at_3i").val();
+
     $.getJSON(
       '/api/v0/business_day',
       { day_count: dueInDays, date: auctionEndedAt, time: (hour + ":" + minute + " " + amPm)  },
