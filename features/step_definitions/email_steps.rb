0ff655d154bd34e2d8d94394e86c9bc698f0ecac
Michael Torres <mtorres253@gmail.com>
2016-08-15 10:45:06 -0400

    Update email_steps.rb
    passing empty string instead of auction obj

--- features/step_definitions/email_steps.rb
+++ features/step_definitions/email_steps.rb
@@ -38,7 +38,7 @@ Then(/^I should receive an email notifying me that I won$/) do
     I18n.t(
       'mailers.auction_mailer.winning_bidder_notification.para_2',
       policy_page_url: faq_url,
-      auction_url: auction_url(auction)
+      auction_url: auction_url("")
     )
   )
 end
