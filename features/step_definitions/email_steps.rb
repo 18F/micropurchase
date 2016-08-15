8d0d642839b026cf7f0a1659cc15e91f2c5f1a00
Michael Torres <mtorres253@gmail.com>
2016-08-15 10:28:24 -0400

    Update email_steps.rb
    passing auction_url to the spec

--- features/step_definitions/email_steps.rb
+++ features/step_definitions/email_steps.rb
@@ -37,7 +37,8 @@ Then(/^I should receive an email notifying me that I won$/) do
   expect(email.body.encoded).to include(
     I18n.t(
       'mailers.auction_mailer.winning_bidder_notification.para_2',
-      policy_page_url: faq_url
+      policy_page_url: faq_url,
+      auction_url: auction_url
     )
   )
 end
