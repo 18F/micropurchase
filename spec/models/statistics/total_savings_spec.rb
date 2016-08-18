43c4c201e59cdcd092b53f794870ef86ad019db1
Jessie A. Young <jessie.a.young@gmail.com>
2016-08-18 15:30:51 -0700

    Calculate savings based on complete AND successful auctions
    * Previously was looking at all complete auctions, resulting in ~$61k on prod
    * Closes #1037

--- spec/models/statistics/total_savings_spec.rb
+++ spec/models/statistics/total_savings_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Statistics::TotalSavings do
   describe '#to_s' do
-    context 'there are completed auctions' do
+    context 'there are complete and successful auctions' do
       it 'returns correct amount' do
         auction = create(:auction, :complete_and_successful, start_price: 10)
         second_auction = create(:auction, :complete_and_successful, start_price: 20)
@@ -13,8 +13,9 @@ describe Statistics::TotalSavings do
       end
     end
 
-    context 'there are no completed auctions' do
+    context 'there are no complete and successful auctions' do
       it 'retrns n/a' do
+        create(:auction, :completed, :with_bidders)
         expect(Statistics::TotalSavings.new.to_s).to eq 'n/a'
       end
     end
