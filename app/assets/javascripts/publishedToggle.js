d78604c7c9b140d547cee4969a1ee47e94e5a081
nikolalsvk <nikolaseap@gmail.com>
2016-08-25 18:06:55 -0500

    Check in jQuery if c2_status is approved

--- app/assets/javascripts/publishedToggle.js
+++ app/assets/javascripts/publishedToggle.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $("#auction_purchase_card").on("change", function(event) {
+    c2_status = $("#auction_c2_status").val()
     published_select_element = $("#auction_published")
 
     published_option = "<option value='published'>published</option>"
     unpublished_option = "<option value='unpublished'>unpublished</option>"
 
-    if(this.value == "default") {
+    if(this.value == "default" && c2_status != "approved") {
       published_select_element.empty().append(unpublished_option)
     } else {
       published_select_element.empty().append(published_option, unpublished_option)
