de9f67645ba73948d29d2d3d2588d918c9719bd8
Jessie A. Young <jessie.a.young@gmail.com>
2016-08-29 10:28:11 -0700

    Fix translations
    * Typo on one
    * Add hyperlink on other

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.raise_delivery_errors = false
+  config.action_view.raise_on_missing_translations = true
   config.active_record.migration_error = :page_load
   config.active_support.deprecation = :log
   config.assets.debug = true
