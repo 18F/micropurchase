361d96f350500a5b2c8e118d0d076a592cc15aa2
Jacob Harris <jacob.harris@gsa.gov>
2016-08-31 14:59:19 -0400

    A few more tweaks to DefaultDeadlineDateTime

--- app/presenters/default_deadline_date_time.rb
+++ app/presenters/default_deadline_date_time.rb
@@ -1,7 +1,12 @@
 class DefaultDeadlineDateTime
-  attr_reader :dc_time
+  attr_reader :start_time, :day_offset
 
   def initialize(start_time:, day_offset:)
-    @dc_time = day_offset.business_days.after(DcTimePresenter.new(start_time).convert)
+    @start_time = start_time
+    @day_offset = day_offset
+  end
+
+  def dc_time
+    day_offset.business_days.after(DcTimePresenter.new(start_time).convert)
   end
 end
--- spec/presenters/default_deadline_date_time_spec.rb
+++ spec/presenters/default_deadline_date_time_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 describe DefaultDeadlineDateTime do
   describe '#initialize' do
     it 'should be N business days later' do
-      t = DcTimePresenter.parse('2016-08-29 15:00')
-      expect(DcTimePresenter.convert(t)).to be_within(1).of(t)
-      expect(DefaultDeadlineDateTime.new(start_time: t, day_offset: 3).dc_time).to be_within(1).of(Time.parse('2016-09-01 15:00 -0400'))
+      time = DcTimePresenter.parse('2016-08-29 15:00')
+      expect(DefaultDeadlineDateTime.new(start_time: time, day_offset: 3).dc_time).to be_within(0.1).of(Time.parse('2016-09-01 15:00 -0400'))
     end
 
     it 'should handle holidays correctly' do
       BusinessTime::Config.holidays << Date.parse('2016-09-05')
-      t = DefaultDateTime.new(Time.parse('2016-08-29 15:00 -0400')).convert
-      expect(DefaultDeadlineDateTime.new(start_time: t, day_offset: 5).dc_time).to be_within(1).of(Time.parse('2016-09-06 13:00 -0400'))
+      time = DefaultDateTime.new(Time.parse('2016-08-29 15:00 -0400')).convert
+      expect(DefaultDeadlineDateTime.new(start_time: time, day_offset: 5).dc_time).to be_within(0.1).of(Time.parse('2016-09-06 13:00 -0400'))
     end
   end
 end
