db36c7689bb352834deee0547b838fcdb00d1c2f
Jacob Harris <jacob.harris@gsa.gov>
2016-09-02 11:27:44 -0400

    Reconciling AdminAuctionStatusPresenterFactory spec

--- spec/models/admin_auction_status_presenter_factory_spec.rb
+++ spec/models/admin_auction_status_presenter_factory_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe AdminAuctionStatusPresenterFactory do
   context 'when the auction approval is not requested' do
-    it 'should return a AdminAuctionStatusPresenter::NotRequested' do
+    it 'should return a C2StatusPresenter::NotRequested' do
       auction = create(:auction, c2_status: :not_requested)
 
       expect(AdminAuctionStatusPresenterFactory.new(auction: auction).create)
@@ -10,17 +10,35 @@ describe AdminAuctionStatusPresenterFactory do
     end
   end
 
-  context 'when the auction approval request is sent' do
-      it 'should return a AdminAuctionStatusPresenter::Sent' do
-        auction = create(:auction, c2_status: :sent)
+  context 'when the auction has been approved' do
+    it 'should return a AdminAuctionStatusPresenter::Accepted' do
+      auction = create(:auction, :closed, :with_bids, :delivered, :accepted)
 
-        expect(AdminAuctionStatusPresenterFactory.new(auction: auction).create)
-          .to be_a(C2StatusPresenter::Sent)
+      expect(AdminAuctionStatusPresenterFactory.new(auction: auction).create)
+        .to be_a(AdminAuctionStatusPresenter::Accepted)
+    end
+  end
+
+  context 'when the auction has been rejected' do
+    it 'should return a AdminAuctionStatusPresenter::Rejected' do
+      auction = create(:auction, :closed, :with_bids, :delivered, :rejected)
+
+      expect(AdminAuctionStatusPresenterFactory.new(auction: auction).create)
+        .to be_a(AdminAuctionStatusPresenter::Rejected)
+    end
+  end
+
+  context 'when the auction approval request is sent' do
+    it 'should return a C2StatusPresenter::Sent' do
+      auction = create(:auction, c2_status: :sent)
+
+      expect(AdminAuctionStatusPresenterFactory.new(auction: auction).create)
+        .to be_a(C2StatusPresenter::Sent)
     end
   end
 
   context 'when the auction approval request is pending' do
-    it 'should return a AdminAuctionStatusPresenter::PendingApproval' do
+    it 'should return a C2StatusPresenter::PendingApproval' do
       auction = create(:auction, c2_status: :pending_approval)
 
       expect(AdminAuctionStatusPresenterFactory.new(auction: auction).create)
@@ -29,7 +47,7 @@ describe AdminAuctionStatusPresenterFactory do
   end
 
   context 'when auction is approved' do
-    it 'should return a AdminAuctionStatusPresenter::Approved' do
+    it 'should return a C2StatusPresenter::Approved' do
       auction = create(:auction, c2_status: :approved)
 
       expect(AdminAuctionStatusPresenterFactory.new(auction: auction).create)
