9e65fa972c0e8f6670fd69faa0ee67ad7e5f471b
Jessie A. Young <jessie.a.young@gmail.com>
2016-09-20 11:52:43 -0700

    Remove `status` dropdown for auction form
    * Update status via bid box only
    * This revealed a bug, which was that we didn't check that a vendor was
      eligible when accepting an auction via the bid box
    * This PR moves the "vendor eligible" code to `AcceptAuction`
    * Closes https://github.com/18F/micropurchase/issues/1243

--- app/controllers/admin/auction_acceptances_controller.rb
+++ app/controllers/admin/auction_acceptances_controller.rb
@@ -2,12 +2,18 @@ class Admin::AuctionAcceptancesController < Admin::BaseController
   def update
     @auction = Auction.find(params[:id])
 
-    AcceptAuction.new(
+    accept_auction = AcceptAuction.new(
       auction: @auction,
       payment_url: winning_bidder.payment_url
-    ).perform
+    )
 
-    @auction.save!
+    if accept_auction.perform
+      @auction.save!
+    else
+      error_messages = @auction.errors.full_messages.to_sentence
+      flash[:error] = error_messages
+    end
+
     redirect_to admin_auction_path(@auction)
   end
 
