c0c4d8c800bf2c1114e49b6fa3772f2a3c4dcd98
Jessie A. Young <jessie.a.young@gmail.com>
2016-09-20 15:55:37 -0700

    Tock band-aid
    * Closes https://github.com/18F/micropurchase/issues/1252
    * This update will allow us to ssh into staging and prod to update tock
      items in our DB
    * Once new auth is implemented on Tock, we will add stuff to access
      auth-d API
    * For now, will run this as a one-off task

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ RSpec.configure do |config|
   config.before do
     mock_github
     WebMock.stub_request(:any, /api.data.gov/).to_rack(FakeSamApi)
-    WebMock.stub_request(:any, /tock-app.18f.gov/).to_rack(FakeTockApi)
+    WebMock.stub_request(:any, /tock.18f.gov/).to_rack(FakeTockApi)
     WebMock.stub_request(:any, /cap.18f.gov/).to_rack(FakeC2Api)
     WebMock.stub_request(:any, /c2-dev.18f.gov/).to_rack(FakeC2Api)
     WebMock.stub_request(:any, /api.github.com/).to_rack(FakeGitHubApi)
