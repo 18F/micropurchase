1262f0a6bf93cd438bee213a6aa5870adff8bcf6
Jessie A. Young <jessie.a.young@gmail.com>
2016-09-22 13:12:22 -0700

    Add footer to admin views
    * Also re-add font-awesome css
      I removed it a while ago thinking it was for fonts, but it was for
      icons like the github icon
    * Closes https://github.com/18F/micropurchase/issues/1270

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.version = (ENV["ASSETS_VERSION"] || "1.0")
 Rails.application.config.assets.precompile += %w( jquery.js )
-Rails.application.config.assets.precompile += %w( uswds/us_flag_small )
+Rails.application.config.assets.precompile += %w( uswds/us_flag_small.png )
