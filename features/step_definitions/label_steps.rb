bf2ed6a855014e1364e8a2ea8382a82b81a86ac8
Jacob Harris <jacob.harris@gsa.gov>
2016-10-11 13:02:56 -0400

    Define the bid label used to available auctions
    I have moved this into the bidding_status presenters and also have tried
    to use the AuctionRules objects to determine winning bidder, etc. This
    only covers available auctions. Future work will clean up closed
    auctions and other bidding statuses.

--- features/step_definitions/label_steps.rb
+++ features/step_definitions/label_steps.rb
@@ -40,6 +40,30 @@ end
 Then(/^I should see a relative closing time for the auction$/) do
   relative_time = DcTimePresenter.new(@auction.ended_at).relative_time
   expect(page).to have_content("Closes #{relative_time}")
+
+Then(/^I should see the current winning bid in a header subtitle$/) do
+  within(:css, '.auction-subtitles') do
+    expect(page).to have_content(
+      I18n.t('bidding_status.available.bid_label.reverse.default',
+             amount: winning_amount,
+             bidder_name: winner_name)
+    )
+  end
+end
+
+Then(/^I should not see the current winning bid in a header subtitle$/) do
+  within(:css, '.auction-subtitles') do
+    expect(page).to_not have_content("Current low bid:")
+  end
+end
+
+Then(/^I should see I have the current winning bid in a header subtitle$/) do
+  within(:css, '.auction-subtitles') do
+    expect(page).to have_content(
+      I18n.t('bidding_status.available.bid_label.reverse.vendor_winning',
+             amount: winning_amount)
+    )
+  end
 end
 
 Then(/^I should see the current winning bid in a header subtitle$/) do
