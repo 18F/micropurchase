3dc68f1d0093ffcefe01ca6770d9056bc956cabf
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-11-17 10:09:59 -0800

    Fixes more manual rubocop low hanging fruit

--- spec/swagger/schema/number_spec.rb
+++ spec/swagger/schema/number_spec.rb
@@ -14,7 +14,12 @@ describe Swagger::Schema::Number do
     end
 
     it 'should include an exclusiveMaximum if specified' do
-      s = Swagger::Schema::Number.new('foo', { 'type' => 'integer', 'exclusiveMaximum' => true, 'maximum' => 3500 }, nil)
+      options = {
+        'type' => 'integer',
+        'exclusiveMaximum' => true,
+        'maximum' => 3500
+      }
+      s = Swagger::Schema::Number.new('foo', options, nil)
       expect(s.displayed_type).to eq('integer (n < 3500)')
     end
 
@@ -29,7 +34,13 @@ describe Swagger::Schema::Number do
     end
 
     it 'should combine maximum and minimums' do
-      s = Swagger::Schema::Number.new('foo', { 'type' => 'integer', 'maximum' => 3500, 'exclusiveMinimum' => true, 'minimum' => 0 }, nil)
+      options = {
+        'type' => 'integer',
+        'maximum' => 3500,
+        'exclusiveMinimum' => true,
+        'minimum' => 0
+      }
+      s = Swagger::Schema::Number.new('foo', options, nil)
       expect(s.displayed_type).to eq('integer (0 < n <= 3500)')
     end
   end
