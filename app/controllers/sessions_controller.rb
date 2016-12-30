95168e2d60b9dfdaed384b1e5a0970bbdccf7fb9
Nick Bristow <Nick.Bristow@gsa.gov>
2016-12-30 17:38:48 -0500

    Added login.gov button Fixed sessions controller to not error out Fixed localhost cert and keys

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = user.id
--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+# require 'rails/commands/server'
+
+# module DefaultOptions
+#   def default_options
+#     super.merge!(Port: 3003)
+#   end
+# end
+
+# Rails::Server.send(:prepend, DefaultOptions)
