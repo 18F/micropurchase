4fe8e7f79ecde0fa7ca2034ef6800facc95dc297
Jessie A. Young <jessieay@users.noreply.github.com>
2017-01-13 10:29:21 -0800

    Test failure case (#1498)
    * Test failure case
    * Before, we did not test this and it was hitting a url that does not
      exist in our app! Silly us.
    * Use consistent indentation

---
+++ spec/support/helpers/omni_auth_helper.rb
@@ -0,0 +1,21 @@
+module OmniAuthHelper
+  def current_user_uid
+    '12345'
+  end
+
+  def mock_github(opts = { })
+    OmniAuth.config.mock_auth[:github] = github_auth_hash(opts)
+  end
+
+  def github_auth_hash(opts = { })
+    OmniAuth::AuthHash.new({
+      provider: 'github',
+      uid: current_user_uid,
+      info: {
+        name: 'Doris Doogooder',
+        email: 'test@example.com',
+        nickname: 'github_username'
+      }
+    }.merge(opts))
+  end
+end
