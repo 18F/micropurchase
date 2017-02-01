1aa69eb722c2c8732aeefcaf596e75656cc9ad7a
James C. Scott <jamesc.scott@gsa.gov>
2017-02-01 14:56:09 -0500

    Migrate Micropurchase Staging to GovCloud
    Micropurchase-Staging has already been migrated to GovCloud and the DNS
    points to the instance on GovCloud. This commit tidys up CI/CD for ONLY
    deploying staging to GovCloud.
    
    More Details:
    - Added GovCloud deployer credentials for staging and production spaces
    in GovCloud
    - Modify travis.yml and deploy.sh to ONLY deploy micropurchase staging
      - Will submit another PR for using the production creds next
    - Removed domain from staging manifest because CDN broker already takes
    care of this.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -15,6 +15,12 @@ if [ $SPACE = 'production' ]; then
 elif [ $SPACE = 'staging' ]; then
   NAME="micropurchase-staging"
   MANIFEST="manifest-staging.yml"
+  CF_PASSWORD=$CF_PASSWORD_STAGING
+  CF_USERNAME=$CF_USERNAME_STAGING
+  # Move two below these to the stop once both staging and
+  # production are deployed to GovCloud.
+  API="https://api.fr.cloud.gov"
+  ORG="gsa-acq-micropurchase"
 else
   echo "Unknown space: $SPACE"
   exit
